b9df2f9090d6715ec7d6ef4f0604f1ba7589423a
Van_Denny <575548935@qq.com>
2018-05-09 08:04:52 +0800

    增加等势线所需功能

--- geo_lab.py
+++ geo_lab.py
@@ -1,4 +1,6 @@
+from Clawer_Base.logger import logger
 from math import radians, cos, sin, asin, sqrt, hypot
+import numpy as np
 import shapefile
 
 class Geo_Point:
@@ -15,6 +17,38 @@ class Geo_Point:
         earth_radius = 6371000
         distance = 2 * asin(sqrt(h)) * earth_radius
         return distance
+
+    def convert_to_rect(self, distance):
+        # distance 单位为m
+        lng_per_meter = 0.00001141
+        lat_per_meter = 0.00000899
+        lng1 = self.lng - (lng_per_meter * distance)
+        lat1 = self.lat - (lat_per_meter * distance)
+        lng2 = self.lng + (lng_per_meter * distance)
+        lat2 = self.lat + (lat_per_meter * distance)
+        return Rectangle(lng1, lat1, lng2, lat2)
+
+    def convert_to_dict(self, key_name):
+        a_dict = {}
+        a_dict[key_name] = str(self.lng)+ ',' + str(self.lat)
+        return a_dict
+
+class Geo_line:
+    def __init__(self,lng1, lat1, lng2, lat2):
+        self.point_1 = Geo_Point(lng1, lat1)
+        self.point_2 = Geo_Point(lng2, lat2)
+        self.length = self.point_1.calc_distance(self.point_2)
+
+    def convert_to_point(self, accuracy):
+        divide = int(self.length / accuracy) + 1
+        lng_min = min([self.point_1.lng, self.point_2.lng])
+        lng_max = max([self.point_1.lng, self.point_2.lng])
+        lat_min = min([self.point_1.lat, self.point_2.lat])
+        lat_max = max([self.point_1.lat, self.point_2.lat])
+        lng_list = np.linspace(lng_min, lng_max, divide)
+        lat_list = np.linspace(lat_min, lat_max, divide)
+        point_list = [Geo_Point(*i) for i in zip(lng_list, lat_list)]
+        return point_list
 
 class Rectangle:
     def __init__(self, lng1=0, lat1=0, lng2=0, lat2=0):
@@ -40,6 +74,43 @@ class Rectangle:
     def out_circle_radius(self):
         radius = hypot(self.wide, self.high)/2
         return radius
+
+    def expand(self, distance, lng1=0, lat1=0, lng2=0, lat2=0):
+        # distance 单位为m
+        lng_per_meter = 0.00001141
+        lat_per_meter = 0.00000899
+        ex_lng1 = round([(self.lng1 - (lng_per_meter * distance)), lng1][lng1 != 0],6)
+        ex_lng2 = round([(self.lng2 + (lng_per_meter * distance)), lng2][lng2 != 0],6)
+        ex_lat1 = round([(self.lat1 - (lat_per_meter * distance)), lat1][lat1 != 0],6)
+        ex_lat2 = round([(self.lat2 + (lat_per_meter * distance)), lat2][lat2 != 0],6)
+        logger.info('拓展矩形为(%s,%s,%s,%s)'%(ex_lng1, ex_lat1, ex_lng2, ex_lat2))
+        return Rectangle(ex_lng1, ex_lat1, ex_lng2, ex_lat2)
+
+    def convert_to_lines(self):
+        self.left_line = Geo_line(self.lng1, self.lat1, self.lng1, self.lat2)
+        self.right_line = Geo_line(self.lng2, self.lat1, self.lng2, self.lat2)
+        self.up_line = Geo_line(self.lng1, self.lat2, self.lng2, self.lat2)
+        self.down_line = Geo_line(self.lng1, self.lat1, self.lng2, self.lat1)
+        return {'left': self.left_line,
+                'right': self.right_line,
+                'up': self.up_line,
+                'down': self.down_line
+                }
+
+    def convert_to_eight_points(self):
+        self.middle_up = Geo_Point(self.center.lng,self.lat2)
+        self.middle_down = Geo_Point(self.center.lng,self.lat1)
+        self.middle_left = Geo_Point(self.lng1,self.center.lat)
+        self.middle_right = Geo_Point(self.lng2, self.center.lat)
+        return [self.left_up,
+                self.left_down,
+                self.right_up,
+                self.right_down,
+                self.middle_up,
+                self.middle_down,
+                self.middle_left,
+                self.middle_right
+               ]
 
     def convert_to_outline_square(self):
         deg_per_meter = (self.lng2 - self.lng1)/self.wide
